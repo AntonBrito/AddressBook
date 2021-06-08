25b39f3f6db62e9841781a77fa0e0de175a43c92
Antonio Alexan Brito <antonioalexanbrito@Antonios-MacBook-Pro.local>
2021-06-07 23:16:31 -0400

    Created address controller and tested routing

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AddressBook.Controllers
+{
+    public class AddressController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+        }
+    }
+}
